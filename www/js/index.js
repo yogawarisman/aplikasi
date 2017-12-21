be0b2d36b0992b071ea712584b9e1431a9675d74
yogawarisman <yoga.warisman@gmail.com>
2017-12-21 09:54:26 +0700

    edit config xml

--- www/js/index.js
+++ www/js/index.js
@@ -40,3 +40,26 @@ var app = {
         
     }
 };
+
+//0=a; 1=b; 2=c; 3=d; 4=e; 
+ correctAnswers=[0,1,2,3,4];
+var AnsweredCorrectly=0;
+	function start(){
+	document.getElementById('quizstart').style.display="none";
+	document.getElementById('question0').style.display="block";
+	}
+	function toggle(a,b){
+	AnsweredCorrectly+=(document.getElementsByName('ans'+a)[correctAnswers[a]].checked?1:-1);
+	document.getElementById('question'+a).style.display="none";
+	document.getElementById('question'+b).style.display="block";
+	}
+	function end(a){
+	AnsweredCorrectly+=(document.getElementsByName('ans'+a)[correctAnswers[a]].checked?1:-1);
+	document.getElementById('question'+a).style.display="none";
+	document.getElementById('summary').style.display="block";
+	document.getElementById('result').innerHTML=AnsweredCorrectly;
+	}
+	function cobalagi(){
+	location.reload(true);
+	AnsweredCorrectly=0;
+	}
