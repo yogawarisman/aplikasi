cfeebdf981cbfbd1500b1c67643bcd3513815f14
yogawarisman <yoga.warisman@gmail.com>
2018-02-28 20:23:39 +0700

    delete yang gak penting

--- www/js/index.js
+++ www/js/index.js
@@ -41,45 +41,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         document.addEventListener("backbutton", onBackKeyDown, false);
-		console.log(Media);
     }
 	// Update DOM on a Received Event
-    receivedEvent: function(id) {
-
-        if( window.plugins && window.plugins.NativeAudio ) {
-
-            var items = ['bass', 'snare', 'highhat', 'bongo'];
-            for(var i=0; i<items.length; i++) {
-                var asset = 'assets/' + items[i] + '.mp3';
-                window.plugins.NativeAudio.preloadSimple(items[i], 
-                                                         asset, 
-                                                         function(msg){console.info(msg)}, 
-                                                         function(msg){ console.error( 'Error: ' + msg ); });
-            }
-
-            window.plugins.NativeAudio.preloadComplex('noise', 
-                                                      'assets/ambient.mp3', 
-                                                      1, // volume
-                                                      1, // voices
-                                                      0, // delay
-            function(msg) {
-                console.info(msg); 
-                window.plugins.NativeAudio.play('noise', 
-                                                function(msg){console.info(msg)}, 
-                                                function(msg){ console.error( 'Error: ' + msg ); }, 
-                                                function(msg){ console.error( 'Complete: ' + msg ); });
-            }, 
-                                                      function(msg){ alert( 'Error: ' + msg ); });
-
-        }
-
-    },
-	play: function(drum) {
-        document.getElementById(drum).classList.add('touched');
-        window.plugins.NativeAudio.play(drum, 
-                                        function(msg){console.info(msg), document.getElementById(drum).classList.remove('touched');},
-                                        function(msg){ console.error( 'Error: ' + msg ); });
-    }
+    receivedEvent: function(id) {    },
 };
 
 //0=a; 1=b; 2=c; 3=d; 4=e; 
